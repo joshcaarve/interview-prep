48e22bf8e08c434c9b84749b031da6f70495a34d
jbrard <Joshuaeli1234@gmail.com>
2022-03-07 13:16:30 -0500

    server success

--- src/server/main.go
+++ src/server/main.go
@@ -7,7 +7,8 @@ import (
 )
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/hello" {
+	log.Println("URL path is: ", r.URL.Path)
+	if r.URL.Path != "/" {
 		http.Error(w, "404 not found.", http.StatusNotFound)
 		return
 	}
@@ -21,7 +22,7 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	http.HandleFunc("/hello", helloHandler)
+	http.HandleFunc("/", helloHandler)
 
 	fmt.Printf("Starting server at port 8080\n")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
